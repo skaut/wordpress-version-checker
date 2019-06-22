7d2c6eea1e218de882d2915059a7db30b00a1199
GenaBitu <developer@dedic.eu>
2019-06-22 22:40:47 +0200

    Getting the readme and the latest version

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import { Application } from 'probot';
 import createScheduler from 'probot-scheduler';
 
-export default app => {
+export default function (app) {
 	createScheduler(app);
-	app.on('schedule.repository', context => {
+	app.on("schedule.repository", function(context) {
+		const options = {
+			host: "api.wordpress.org",
+			port: 443,
+			path: "/core/stable-check/1.0/"
+		};
+		const readme = context.repos.getContents({
+			owner: "skaut",
+			repo: "skaut-google-drive-gallery",
+			path: "plugin/readme.txt"
+		});
+		context.log(readme);
+		http.get(options, function(response) {
+			const list = JSON.parse(response);
+			const latest = Object.keys(list).find(key => list[key] === "latest");
+		});
 	});
 };
 
