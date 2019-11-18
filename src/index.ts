fa9ec9ef2611495fcadb8ce8df12a275545232e0
GenaBitu <developer@dedic.eu>
2019-11-18 10:46:08 +0100

    Added better check for existing issues

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ function createIssue(testedVersion: string, latestVersion: string): void
 {
 	const args = {
 		...repo,
-		title: "The plugin hasn't been tested with the latest version of WordPress",
+		title: "[wpvc] The plugin hasn't been tested with the latest version of WordPress",
 		body: 'There is a new WordPress version that the plugin hasn\'t been tested with. Please test it and then change the "Tested up to" field in the plugin readme.\n\n**Tested up to:** ' + testedVersion + '\n**Latest version:** ' + latestVersion + '\n\nYou may then close this issue as it won\'t be done automatically.'
 	};
 	octokit.issues.create(args);
@@ -18,8 +18,11 @@ function createIssue(testedVersion: string, latestVersion: string): void
 
 function outdated(testedVersion: string, latestVersion: string): void
 {
-	octokit.issues.listForRepo({...repo, creator: 'wordpress-version-checker[bot]'}).then(function(result): void {
-		if(result.data.length === 0)
+	octokit.issues.listForRepo({...repo, creator: 'github-actions[bot]'}).then(function(result): void {
+		const wpvcIssues = result.data.filter(function(issue) {
+			return issue.title.startsWith('[wpvc]')
+		});
+		if(wpvcIssues.length === 0)
 		{
 			createIssue(testedVersion, latestVersion);
 		}
