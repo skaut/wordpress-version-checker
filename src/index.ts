e463e797e4e38b9e65dbd9455465e3d795adec47
Marek Dědič <developer@dedic.eu>
2021-02-06 19:01:28 +0100

    Error handling

--- src/index.ts
+++ src/index.ts
@@ -41,13 +41,22 @@ function createIssue(testedVersion: string, latestVersion: string): void
 }
 
 function updateIssue(issue: number, testedVersion: string, latestVersion: string) {
-	void octokit.issues.get({...repo, issue_number: issue}).then(function(result) { // TODO: catch
-		const latestVersionInIssue = result.data.body.split('\r\n').find(function(line) {
+	octokit.issues.get({...repo, issue_number: issue}).then(function(result) {
+		const matchingLine = result.data.body.split('\r\n').find(function(line) {
 			return line.startsWith('**Latest version:**');
-		})!.slice(20);
-		if(compareVersions.compare(latestVersionInIssue, latestVersion, '<')) {
-			void octokit.issues.update({...repo, issue_number: issue, body: issueBody(testedVersion, latestVersion)}) // TODO
+		})
+		if(!matchingLine) {
+			console.log('Existing issue for repository ' + repoName + ' doesn\'t have the correct format.');
+			return;
 		}
+		const latestVersionInIssue = matchingLine.slice(20);
+		if(compareVersions.compare(latestVersionInIssue, latestVersion, '<')) {
+			octokit.issues.update({...repo, issue_number: issue, body: issueBody(testedVersion, latestVersion)}).catch(function(e) {
+				console.log('Couldn\'t update existing issue for repository ' + repoName + '. Error message: ' + String(e));
+			});
+		}
+	}).catch(function(e): void {
+		console.log('Couldn\'t get existing issue for repository ' + repoName + '. Error message: ' + String(e));
 	});
 }
 
