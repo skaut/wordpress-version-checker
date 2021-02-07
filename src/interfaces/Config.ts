2e56915dec5943562f2955752f6508f8c9fc47d6
Marek Dědič <developer@dedic.eu>
2021-02-07 23:10:07 +0100

    Split config getting

--- src/interfaces/Config.ts
+++ src/interfaces/Config.ts
@@ -1,11 +1,3 @@
 export interface Config {
 	readme: string
 }
-
-export function isConfig(config: Record<string, unknown>): config is Record<string, unknown> & Config {
-	if(!config.readme)
-	{
-		return false;
-	}
-	return true;
-}
--- src/tested-version.ts
+++ src/tested-version.ts
@@ -1,41 +1,11 @@
-import { Config, isConfig } from './interfaces/Config'
 import { octokit } from './octokit';
 import { repo } from './repo';
 
+import {WPVCConfig} from './wpvc-config';
+import { hasStatus } from './has-status';
+
 import {ConfigError} from './exceptions/ConfigError';
 import {InvalidReadmeError} from './exceptions/InvalidReadmeError';
-
-function hasStatus(obj: Record<string, unknown>): obj is Record<"status", unknown> {
-	return Object.prototype.hasOwnProperty.call(obj, "status")
-}
-
-async function WPVCConfig(): Promise<Config|null> {
-	const file = await octokit.repos.getContent({...repo, path: '.wordpress-version-checker.json'}).catch(function(e): null|never {
-		if(hasStatus(e) && e.status === 404) {
-			return null;
-		} else {
-			throw new ConfigError(String(e));
-		}
-	});
-	if(file === null) {
-		return null;
-	}
-	const encodedContent = (file.data as {content?: string}).content;
-	if(!encodedContent) {
-		throw new ConfigError('Failed to decode the file.');
-	}
-	let config: Record<string, unknown> = {};
-	try {
-		config = JSON.parse(Buffer.from(encodedContent, 'base64').toString()) as Record<string, unknown>;
-	} catch(e) {
-		throw new ConfigError((e as SyntaxError).message);
-	}
-	if(!isConfig(config))
-	{
-		throw new ConfigError('Invalid config file.');
-	}
-	return config;
-}
 
 async function readme(): Promise<string>
 {
