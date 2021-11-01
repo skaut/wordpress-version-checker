4ec3294cd878e61456357cbfb687f6d1eb654f64
Marek Dědič <developer@dedic.eu>
2021-11-01 22:59:43 +0100

    Added nock mocking

--- __tests__/latest-version.test.ts
+++ __tests__/latest-version.test.ts
@@ -1,5 +1,14 @@
+import nock from "nock";
+
 import { latestWordPressVersion } from "../src/latest-version";
 
+//nock.recorder.rec();
+
 test("latestWordPressVersion works correctly", async () => {
-  await expect(latestWordPressVersion()).resolves.toStrictEqual("5.8");
+  nock("https://api.wordpress.org").get("/core/stable-check/1.0/").reply(200, {
+    "0.40": "insecure",
+    "0.41": "outdated",
+    "0.42": "latest",
+  });
+  await expect(latestWordPressVersion()).resolves.toStrictEqual("0.42");
 });
