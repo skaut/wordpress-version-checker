0cd2cea20508ffc7e87c841b0d9ce9676ecbcdb2
Marek Dědič <developer@dedic.eu>
2021-11-01 22:02:10 +0100

    Lint fix on test file

--- __tests__/latest-version.test.ts
+++ __tests__/latest-version.test.ts
@@ -1,5 +1,5 @@
 import { latestWordPressVersion } from "../src/latest-version";
 
-test("latestWordPressVersion works correctly", () => {
-	expect(latestWordPressVersion()).resolves.toStrictEqual("5.8")
+test("latestWordPressVersion works correctly", async () => {
+  await expect(latestWordPressVersion()).resolves.toStrictEqual("5.8");
 });
