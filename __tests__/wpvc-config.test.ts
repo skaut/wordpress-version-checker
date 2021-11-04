a565d974e1c6ae1e879e4ba16d6daa5ab1457982
Marek Dědič <developer@dedic.eu>
2021-11-04 19:06:07 +0100

    All tests for wpvc-config

--- __tests__/wpvc-config.test.ts
+++ __tests__/wpvc-config.test.ts
@@ -6,6 +6,8 @@ import * as core from "@actions/core";
 
 import { WPVCConfig } from "../src/wpvc-config";
 
+import { ConfigError } from "../src/exceptions/ConfigError";
+
 jest.mock("@actions/core");
 
 describe("Mocked env variables", () => {
@@ -31,5 +33,64 @@ describe("Mocked env variables", () => {
       });
 
     await expect(WPVCConfig()).resolves.toStrictEqual(config);
+  });
+
+  test("WPVCConfig works correctly with assignees", async () => {
+    const config = {
+      readme: "path/to/readme.txt",
+      assignees: ["PERSON1", "PERSON2"],
+    };
+
+    nock("https://api.github.com")
+      .get("/repos/OWNER/REPO/contents/.wordpress-version-checker.json")
+      .reply(200, {
+        content: Buffer.from(JSON.stringify(config)).toString("base64"),
+      });
+
+    await expect(WPVCConfig()).resolves.toStrictEqual(config);
+  });
+
+  test("WPVCConfig fails gracefully on connection issues", async () => {
+    await expect(WPVCConfig()).rejects.toThrow(ConfigError);
+  });
+
+  test("WPVCConfig returns null on no config", async () => {
+    nock("https://api.github.com")
+      .get("/repos/OWNER/REPO/contents/.wordpress-version-checker.json")
+      .reply(404);
+
+    await expect(WPVCConfig()).resolves.toStrictEqual(null);
+  });
+
+  test("WPVCConfig fails gracefully on invalid response", async () => {
+    nock("https://api.github.com")
+      .get("/repos/OWNER/REPO/contents/.wordpress-version-checker.json")
+      .reply(200);
+
+    await expect(WPVCConfig()).rejects.toThrow(ConfigError);
+  });
+
+  test("WPVCConfig fails gracefully on invalid response 2", async () => {
+    nock("https://api.github.com")
+      .get("/repos/OWNER/REPO/contents/.wordpress-version-checker.json")
+      .reply(200, {
+        content: "OOPS",
+      });
+
+    await expect(WPVCConfig()).rejects.toThrow(ConfigError);
+  });
+
+  test("WPVCConfig fails gracefully on invalid config", async () => {
+    const config = {
+      readme_incorrect: "path/to/readme.txt",
+    };
+
+    nock("https://api.github.com")
+      .get("/repos/OWNER/REPO/contents/.wordpress-version-checker.json")
+      .reply(200, {
+        content: Buffer.from(JSON.stringify(config)).toString("base64"),
+      });
+
+    await expect(WPVCConfig()).rejects.toThrow(ConfigError);
   });
 });
