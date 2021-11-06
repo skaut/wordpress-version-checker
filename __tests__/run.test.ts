4f5ec9921961e96d70c065cc0ed1296e090b1122
Marek Dědič <developer@dedic.eu>
2021-11-06 15:53:01 +0100

    Added tests for all successful run invocations

--- __tests__/run.test.ts
+++ __tests__/run.test.ts
@@ -1,8 +1,11 @@
-//import nock from "nock";
 import { mocked } from "ts-jest/utils";
-//import mockedEnv from "mocked-env";
 
-import { createIssue, getIssue, updateIssue } from "../src/issue-management";
+import {
+  closeIssue,
+  createIssue,
+  getIssue,
+  updateIssue,
+} from "../src/issue-management";
 import { latestWordPressVersion } from "../src/latest-version";
 import { testedVersion } from "../src/tested-version";
 import { WPVCConfig } from "../src/wpvc-config";
@@ -14,14 +17,10 @@ jest.mock("../src/tested-version");
 jest.mock("../src/wpvc-config");
 
 describe("[env variable mock]", () => {
-  //let restore: () => void;
-
   beforeEach(() => {
-    //restore = mockedEnv({ GITHUB_REPOSITORY: "OWNER/REPO" });
-    //mocked(core).getInput.mockReturnValue("GH_TOKEN");
-  });
-  afterEach(() => {
-    //restore();
+    mocked(closeIssue).mockResolvedValue(undefined);
+    mocked(createIssue).mockResolvedValue(undefined);
+    mocked(updateIssue).mockResolvedValue(undefined);
   });
 
   test("run works correctly with outdated version and no existing issue", async () => {
@@ -29,17 +28,19 @@ describe("[env variable mock]", () => {
     const testedVersionValue = "0.41";
     const latestVersionValue = "0.42";
 
-    mocked(createIssue).mockResolvedValue(undefined);
     mocked(getIssue).mockResolvedValue(null);
     mocked(latestWordPressVersion).mockResolvedValue(latestVersionValue);
     mocked(testedVersion).mockResolvedValue(testedVersionValue);
     mocked(WPVCConfig).mockResolvedValue(config);
+
     await run();
 
+    expect(mocked(closeIssue).mock.calls.length).toBe(0);
     expect(mocked(createIssue).mock.calls.length).toBe(1);
     expect(mocked(createIssue).mock.calls[0][0]).toStrictEqual(config);
     expect(mocked(createIssue).mock.calls[0][1]).toBe(testedVersionValue);
     expect(mocked(createIssue).mock.calls[0][2]).toBe(latestVersionValue);
+    expect(mocked(updateIssue).mock.calls.length).toBe(0);
   });
 
   test("run works correctly with outdated version and an existing issue", async () => {
@@ -48,16 +49,54 @@ describe("[env variable mock]", () => {
     const latestVersionValue = "0.42";
     const existingIssue = 123;
 
-    mocked(updateIssue).mockResolvedValue(undefined);
     mocked(getIssue).mockResolvedValue(existingIssue);
     mocked(latestWordPressVersion).mockResolvedValue(latestVersionValue);
     mocked(testedVersion).mockResolvedValue(testedVersionValue);
     mocked(WPVCConfig).mockResolvedValue(config);
+
     await run();
 
+    expect(mocked(closeIssue).mock.calls.length).toBe(0);
+    expect(mocked(createIssue).mock.calls.length).toBe(0);
     expect(mocked(updateIssue).mock.calls.length).toBe(1);
     expect(mocked(updateIssue).mock.calls[0][0]).toBe(existingIssue);
     expect(mocked(updateIssue).mock.calls[0][1]).toBe(testedVersionValue);
     expect(mocked(updateIssue).mock.calls[0][2]).toBe(latestVersionValue);
+  });
+
+  test("run works correctly with up-to-date version and no existing issue", async () => {
+    const config = { readme: "readme.txt" };
+    const testedVersionValue = "0.42";
+    const latestVersionValue = "0.42";
+
+    mocked(getIssue).mockResolvedValue(null);
+    mocked(latestWordPressVersion).mockResolvedValue(latestVersionValue);
+    mocked(testedVersion).mockResolvedValue(testedVersionValue);
+    mocked(WPVCConfig).mockResolvedValue(config);
+
+    await run();
+
+    expect(mocked(closeIssue).mock.calls.length).toBe(0);
+    expect(mocked(createIssue).mock.calls.length).toBe(0);
+    expect(mocked(updateIssue).mock.calls.length).toBe(0);
+  });
+
+  test("run works correctly with up-to-date version and an existing issue", async () => {
+    const config = { readme: "readme.txt" };
+    const testedVersionValue = "0.42";
+    const latestVersionValue = "0.42";
+    const existingIssue = 123;
+
+    mocked(getIssue).mockResolvedValue(existingIssue);
+    mocked(latestWordPressVersion).mockResolvedValue(latestVersionValue);
+    mocked(testedVersion).mockResolvedValue(testedVersionValue);
+    mocked(WPVCConfig).mockResolvedValue(config);
+
+    await run();
+
+    expect(mocked(closeIssue).mock.calls.length).toBe(1);
+    expect(mocked(closeIssue).mock.calls[0][0]).toBe(existingIssue);
+    expect(mocked(createIssue).mock.calls.length).toBe(0);
+    expect(mocked(updateIssue).mock.calls.length).toBe(0);
   });
 });
