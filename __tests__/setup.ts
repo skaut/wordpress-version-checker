fbdb8048a13fe4998a5a1a45f5f655ecd35d67f0
Marek Dědič <developer@dedic.eu>
2023-11-14 15:35:42 +0100

    Stubbing octokit in tests

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,3 +1,23 @@
-import * as nock from "nock";
+import core from "@actions/core";
+import github from "@actions/github";
+import nock from "nock";
+import type { Octokit } from "@octokit/core";
+import type { Api } from "@octokit/plugin-rest-endpoint-methods/dist-types/types";
+import type { PaginateInterface } from "@octokit/plugin-paginate-rest";
+import nodeFetch from "node-fetch";
 
 nock.disableNetConnect();
+
+type GitHub = Api & Octokit & { paginate: PaginateInterface };
+
+jest.mock("../src/octokit", () => {
+  return {
+    octokit(): GitHub {
+      return github.getOctokit(core.getInput("repo-token"), {
+        request: {
+          fetch: nodeFetch,
+        },
+      });
+    },
+  };
+});
