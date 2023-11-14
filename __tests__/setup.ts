69ca6230d89194aa1d8ebe3bf5e374a485a41301
Marek Dědič <developer@dedic.eu>
2023-11-14 15:35:42 +0100

    Stubbing octokit in tests

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,3 +1,20 @@
-import * as nock from "nock";
+import core from "@actions/core";
+import * as github from "@actions/github";
+import type { Octokit } from "@octokit/core";
+import type { PaginateInterface } from "@octokit/plugin-paginate-rest";
+import type { Api } from "@octokit/plugin-rest-endpoint-methods/dist-types/types";
+import nock from "nock";
+import nodeFetch from "node-fetch";
 
 nock.disableNetConnect();
+
+type GitHub = Api & Octokit & { paginate: PaginateInterface };
+
+jest.mock("../src/octokit", () => ({
+  octokit: (): GitHub =>
+    github.getOctokit(core.getInput("repo-token"), {
+      request: {
+        fetch: nodeFetch,
+      },
+    }),
+}));
--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,16 @@ export default {
   coverageDirectory: "coverage",
   coverageProvider: "babel",
   setupFiles: ["<rootDir>/__tests__/setup.ts"],
-  preset: "ts-jest/presets/default",
+  transform: {
+    // eslint-disable-next-line @typescript-eslint/naming-convention -- The key is a glob.
+    "^.+\\.[jt]s$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/test.tsconfig.json",
+      },
+    ],
+  },
+  transformIgnorePatterns: ["node_modules/(?!node-fetch)/"],
   resetMocks: true,
   testMatch: ["<rootDir>/__tests__/**/*.test.ts"],
 };
