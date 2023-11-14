1ad3b4f35ccd1326b0645006c0e13d2e8a31f193
Marek Dědič <developer@dedic.eu>
2023-11-14 15:43:57 +0100

    Lint fix

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,23 +1,20 @@
 import core from "@actions/core";
 import github from "@actions/github";
-import nock from "nock";
 import type { Octokit } from "@octokit/core";
-import type { Api } from "@octokit/plugin-rest-endpoint-methods/dist-types/types";
 import type { PaginateInterface } from "@octokit/plugin-paginate-rest";
+import type { Api } from "@octokit/plugin-rest-endpoint-methods/dist-types/types";
+import nock from "nock";
 import nodeFetch from "node-fetch";
 
 nock.disableNetConnect();
 
 type GitHub = Api & Octokit & { paginate: PaginateInterface };
 
-jest.mock("../src/octokit", () => {
-  return {
-    octokit(): GitHub {
-      return github.getOctokit(core.getInput("repo-token"), {
-        request: {
-          fetch: nodeFetch,
-        },
-      });
-    },
-  };
-});
+jest.mock("../src/octokit", () => ({
+  octokit: (): GitHub =>
+    github.getOctokit(core.getInput("repo-token"), {
+      request: {
+        fetch: nodeFetch,
+      },
+    }),
+}));
