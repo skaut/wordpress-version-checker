485ac5ae180b758cd630e9742cdf370a6484bbb7
Marek Dědič <developer@dedic.eu>
2024-02-19 00:06:54 +0100

    Removed sources

--- src/wpvc-config.ts
+++ src/wpvc-config.ts
@@ -1,7 +1,8 @@
-import { octokit } from "./octokit";
+import * as github from "@actions/github";
 
 export async function WPVCConfig(): Promise<boolean> {
-  const file = await octokit().rest.repos.getContent({
+  const octokit = github.getOctokit("");
+  const file = await octokit.rest.repos.getContent({
     owner: "",
     repo: "",
     path: "",
